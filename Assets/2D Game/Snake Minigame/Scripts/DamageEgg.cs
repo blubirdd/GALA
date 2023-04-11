9f10203c4e6117442f11c4339b955db98e752e49
AJL <chubbybmo@gmail.com>
2023-04-11 13:51:22 +0800

    AJL updated 2d minigames
    AJL has updated snake and mole minigame

---
+++ Assets/2D Game/Snake Minigame/Scripts/DamageEgg.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEgg : MonoBehaviour
+{
+    public float damage;
+    private HealthBar pHealth;
+
+    void Start()
+    {
+        // Find the object with the HealthBar component and assign it to pHealth
+        pHealth = FindObjectOfType<HealthBar>();
+        if (pHealth == null)
+        {
+            Debug.LogError("No HealthBar found in the scene!");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Egg"))
+        {
+            pHealth.health -= damage;
+        }
+    }
+}
